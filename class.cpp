9a6a2942b7bbdd206fa76f3b0e9dfc72a716b124
Kalwinzi masuluzu chapa <kalwinzic@gmail.com>
2025-01-02 10:12:18 +0300

    Add files via upload
    Class for BST

---
+++ class.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int value;
+    Node* left;
+    Node* right;
+
+    Node(int val) : value(val), left(nullptr), right(nullptr) {}
+};
